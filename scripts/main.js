ffea63746f22e55fc95ed2395ed1193acb1acd05
Zakini <jesus.zakini@gmail.com>
2017-01-13 12:36:12 +0000

    Added shim config for bootstrap since it's not an AMD module

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,12 @@ requirejs.config({
 		'underscore': ['https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min', 'lib/underscore-min'],
 		'backbone': ['https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min', 'lib/backbone.min'],
 		'bootstrap': ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min', 'lib/bootstrap.min']
-    }
+    },
+	shim: {
+		'bootstrap': {
+			deps: ['jquery']
+		}
+	}
 });
 
 require(['jquery', 'underscore', 'backbone', 'bootstrap'], function ($, _, Backbone, bootstrap) {
