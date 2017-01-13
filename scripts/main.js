01a2f608e45edc917a17c263626d5739ec00b87f
Zakini <jesus.zakini@gmail.com>
2017-01-13 12:31:39 +0000

    Set up libraries (should be all the ones I need)

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+requirejs.config({
+    paths: {
+        'jquery': ['https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min', 'lib/jquery.min'],
+		'underscore': ['https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min', 'lib/underscore-min'],
+		'backbone': ['https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min', 'lib/backbone.min'],
+		'bootstrap': ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min', 'lib/bootstrap.min']
+    }
+});
+
+require(['jquery', 'underscore', 'backbone', 'bootstrap'], function ($, _, Backbone, bootstrap) {
+	'use strict';
+
+    console.log($);
+	console.log('loaded jquery');
+
+	console.log(_);
+	console.log('loaded underscore');
+
+	console.log(Backbone);
+	console.log('loaded backbone');
+
+	console.log(bootstrap);
+	console.log('loaded bootstrap');
+});
